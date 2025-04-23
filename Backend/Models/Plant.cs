2cbdfb64072abb8e271218e87d5049a52ba374ea
Simon VIlslev <148044448+simo100f@users.noreply.github.com>
2025-04-23 08:02:16 +0200

    Updated models

--- Backend/Models/Plant.cs
+++ Backend/Models/Plant.cs
@@ -6,7 +6,15 @@ public class Plant
 {
     [Key]
     public int Id { get; set; }
-    public string Name { get; set; }
-   
+    public string Name { get; set; } = "";
+    public float IdealSoilMoisture { get; set; }
+    public float IdealTemperature { get; set; }
+    public float IdealLightLevel { get; set; }
+    public float IdealAirHumidity { get; set; }
+    public float SoilMoisture { get; set; }
+    public float Temperature { get; set; }
+    public float LightLevel { get; set; }
+    public float AirHumidity { get; set; }
+    public string UserEmail { get; set; } = "";
     
 }
