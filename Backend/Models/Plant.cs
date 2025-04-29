1f4eb7246a857c0d5d3fc59ef31e3e33483a3f4f
Yevheniia Zhygalova <eva.lypak@gmail.com>
2025-04-29 13:55:06 +0200

    add Metrics table and Arduino script

--- Backend/Models/Plant.cs
+++ Backend/Models/Plant.cs
@@ -6,15 +6,14 @@ public class Plant
 {
     [Key]
     public string GUID { get; set; } = "";
-    public string Name { get; set; } = "";
+    public string PlantName { get; set; } = "";
     public float IdealSoilMoisture { get; set; }
     public float IdealTemperature { get; set; }
     public float IdealLightLevel { get; set; }
     public float IdealAirHumidity { get; set; }
-    public float SoilMoisture { get; set; }
-    public float Temperature { get; set; }
-    public float LightLevel { get; set; }
-    public float AirHumidity { get; set; }
     public string UserEmail { get; set; } = "";
     
+    // Navigation property: one Plant has many Metrics
+    public List<Metric> Metrics { get; set; } = new();
+
 }
