03232614e717a45f048b8ccc125cdd57d4831fbc
Lukas11n <luno0001@edu.mercantec.dk>
2025-04-29 15:40:22 +0200

    login og signup virker

---
+++ PotPalFrontend/Repo/UserRepo.cs
@@ -0,0 +1,34 @@
+﻿using Models;
+using PotPalFrontend.Services;
+
+namespace PotPalFrontend.Repo
+{
+    public class UserRepo
+    {
+      
+
+        UserServices userServices = new UserServices();
+
+        public async Task<UserDTO> LoginAsync(UserLoginDTO login)
+        {
+            if (login == null || string.IsNullOrEmpty(login.Email) || string.IsNullOrEmpty(login.Password)) return null; 
+
+            //Stores the login data in userDTO
+            UserDTO userDTO = await userServices.LoginAsync(login);
+
+            return userDTO != null ? userDTO : null;
+        }
+
+        public async Task<UserDTO> AddAsync(CreateUserDTO signUp)
+        {
+            if (signUp == null || string.IsNullOrEmpty(signUp.Email) || string.IsNullOrEmpty(signUp.UserName) || string.IsNullOrEmpty(signUp.Password)) return null;
+
+
+            UserDTO userDTO = await userServices.AddAsync(signUp);
+
+            return userDTO != null ? userDTO : null;
+        }
+
+        
+    }
+}
