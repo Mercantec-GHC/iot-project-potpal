3fe7455b513d206f2cd5c037109a600367877d2a
Simon VIlslev <148044448+simo100f@users.noreply.github.com>
2025-05-07 09:06:07 +0200

    Update Program.cs

--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using System.Text.Json.Serialization;
 using System.Text;
 
 
