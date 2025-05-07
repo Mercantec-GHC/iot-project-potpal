c69832bf45a73d9d996c0b61ae0b922cc1de7fe9
Simon VIlslev <148044448+simo100f@users.noreply.github.com>
2025-05-07 10:00:51 +0200

    Update projects

---
+++ Frontend/Frontend/Components/Pages/Signup.razor.cs
@@ -0,0 +1,23 @@
+﻿using Models;
+using PotPalFrontend.Repo;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Principal;
+
+namespace PotPalFrontend.Components.Pages
+{
+    public partial class Signup
+    {
+        public CreateUserDTO userCreate { get; set; } = new CreateUserDTO();
+
+        public UserDTO UserResult { get; set; }
+
+        private UserRepo userRepo = new UserRepo();
+
+        public async Task ConfirmSignUp()
+        {
+            UserResult = await userRepo.AddAsync(userCreate);
+            StateHasChanged();
+        }
+
+    }
+}
